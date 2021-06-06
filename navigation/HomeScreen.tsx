bcad87fc04abd0ce5dbe481db2f7c3398bd2ef52
Sebastian Labadie <sebastianlabadie.contact@gmail.com>
2021-06-06 02:28:16 -0300

    flatlist of videos

--- navigation/HomeScreen.tsx
+++ navigation/HomeScreen.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import {View,StyleSheet,Text} from 'react-native'
+import {View,StyleSheet,Text, FlatList} from 'react-native'
 import VideoCard from '../components/VideoCard/VideoCard'
 import videos from '../assets/data/videos.json'
 function HomeScreen() {
     return (
         <View>
-            <VideoCard video={videos[0]} />
-            <VideoCard video={videos[1]} />
+           <FlatList keyExtractor={(item)=>item.id} data={videos} renderItem={({item})=><VideoCard video={item} />}  />
         </View>
     )
 }
